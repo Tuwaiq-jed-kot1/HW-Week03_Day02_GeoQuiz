d78023c24a5350e59058167ee0d94794ec7d4bae
Abeer <abeeralghamdi.jed.k1@tuwaiq.edu.sa>
2021-10-12 00:28:59 +0300

    was a fun HW :)

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.geoquiz
-
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -12,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
+    private lateinit var prevButton: Button
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -32,6 +34,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        prevButton =findViewById(R.id.prev_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
@@ -44,6 +47,10 @@ class MainActivity : AppCompatActivity() {
 
         nextButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+        prevButton.setOnClickListener {
+            currentIndex = (currentIndex - 1) % questionBank.size
             updateQuestion()
         }
 
@@ -64,5 +71,13 @@ class MainActivity : AppCompatActivity() {
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
             .show()
+        val myToast : Toast =  Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+        val txt: TextView? =  myToast.view?.findViewById(android.R.id.message)
+        txt?.setTextColor(Color.WHITE)
+        txt?.setBackgroundColor(Color.BLUE)
+        myToast.setGravity(Gravity.TOP,0,300)
+        myToast.show()
     }
 }
+
+
