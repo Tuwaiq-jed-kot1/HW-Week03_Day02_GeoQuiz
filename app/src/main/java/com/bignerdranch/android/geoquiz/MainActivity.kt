093a73203fd188a79332440dd3c584cc00d228ad
Hani <hani.dabsh.jed.k1@tuwaiq.edu.sa>
2021-10-11 23:12:34 +0300

    Nice and smooth homework

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -6,12 +6,15 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import android.view.Gravity
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
+    private lateinit var prevButton : Button
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -32,6 +35,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.prev_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
@@ -47,8 +51,20 @@ class MainActivity : AppCompatActivity() {
             updateQuestion()
         }
 
+        prevButton.setOnClickListener {
+            if(currentIndex == 0){
+                currentIndex = (questionBank.size-1)
+                updateQuestion()
+            }else{
+                currentIndex = (currentIndex - 1) % questionBank.size
+                updateQuestion()
+            }
+        }
+
         updateQuestion()
     }
+
+
 
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
@@ -62,7 +78,8 @@ class MainActivity : AppCompatActivity() {
         } else {
             R.string.incorrect_toast
         }
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
-            .show()
+        val toast: Toast = Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.TOP, 0, 20)
+        toast.show()
     }
 }
