9671f0564b5df005c6b15e5d028c76dce1195a3f
faisalha98 <fisal.alharthi.jed.k1@tuwaiq.edu.sa>
2021-10-11 20:52:57 +0300

    nice

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.bignerdranch.android.geoquiz
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -12,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
+    private lateinit var backButton: Button
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -32,6 +35,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        backButton = findViewById(R.id.back_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
@@ -44,6 +48,10 @@ class MainActivity : AppCompatActivity() {
 
         nextButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+        backButton.setOnClickListener {
+            currentIndex = (currentIndex - 1) % questionBank.size
             updateQuestion()
         }
 
@@ -62,7 +70,14 @@ class MainActivity : AppCompatActivity() {
         } else {
             R.string.incorrect_toast
         }
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
-            .show()
+        Toast.makeText(this, messageResId, Toast.LENGTH_LONG)
+        val Toast1 : Toast = Toast.makeText(this,messageResId,Toast.LENGTH_LONG)
+        val textToast: TextView? = Toast1.view?.findViewById(android.R.id.message)
+        textToast?.setTextColor(Color.BLUE)
+        textToast?.setBackgroundColor(Color.LTGRAY)
+        textToast?.setTextSize(20F)
+        Toast1.setGravity(Gravity.TOP ,0,350)
+        Toast1.show()
+
     }
 }
