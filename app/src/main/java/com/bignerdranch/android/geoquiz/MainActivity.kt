f4ec07d2c812b1f6bc7114bafaabfc71c42e17af
Basil19 <bojsl-9@hotmail.com>
2021-10-12 01:23:07 +0300

    DONE:)

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.bignerdranch.android.geoquiz
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -12,6 +14,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
+    private lateinit var previosButton:Button
+
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -32,6 +36,8 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        previosButton=findViewById(R.id.btnPrevious)
+
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
@@ -47,22 +53,43 @@ class MainActivity : AppCompatActivity() {
             updateQuestion()
         }
 
+        previosButton.setOnClickListener {
+
+            currentIndex=(currentIndex-1) % questionBank.lastIndex
+            Toast.makeText(this,"$currentIndex",Toast.LENGTH_SHORT).show()
+
+            updateQuestion()
+
+
+        }
+
         updateQuestion()
     }
+
 
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
     }
 
-    private fun checkAnswer(userAnswer: Boolean) {
-        val correctAnswer = questionBank[currentIndex].answer
-        val messageResId = if (userAnswer == correctAnswer) {
-            R.string.correct_toast
-        } else {
-            R.string.incorrect_toast
-        }
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
-            .show()
-    }
+    private fun checkAnswer(check: Boolean) {
+        //cheack if true or false
+
+        val correctAnswer=questionBank[currentIndex].answer
+        val messageAnswer=if (check == correctAnswer)R.string.correct_toast else R.string.incorrect_toast
+
+
+        val toast1=Toast.makeText(this,getString(messageAnswer),Toast.LENGTH_SHORT)
+        val _toast1:TextView = toast1.view!!.findViewById(android.R.id.message)
+        toast1.setGravity(Gravity.CENTER,5,5)
+        if (check==correctAnswer){
+            _toast1.setTextColor(Color.GREEN)
+            _toast1.setTextSize(24F)
+            toast1.show()
+        }else{
+            _toast1.setTextColor(Color.RED)
+            _toast1.setTextSize(24F)
+            toast1.show()
+        } }
+
 }
