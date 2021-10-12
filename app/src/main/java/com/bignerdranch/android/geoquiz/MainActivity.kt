945a74766c06958d7b8b47785b53166fc3cd44e7
NadiaAliAlmrashi <Nadiyah.Almurashi.jed.k1@tuwaiq.edu.sa>
2021-10-12 03:19:14 +0300

    good

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.bignerdranch.android.geoquiz
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -12,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
+    private lateinit var previosButton: Button
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -46,7 +49,10 @@ class MainActivity : AppCompatActivity() {
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
         }
-
+        previosButton.setOnClickListener {
+            currentIndex = (currentIndex - 1) % questionBank.lastIndex
+            Toast.makeText(this, "$currentIndex", Toast.LENGTH_SHORT).show()
+        }
         updateQuestion()
     }
 
@@ -56,13 +62,35 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
-        val correctAnswer = questionBank[currentIndex].answer
-        val messageResId = if (userAnswer == correctAnswer) {
-            R.string.correct_toast
-        } else {
-            R.string.incorrect_toast
-        }
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
-            .show()
+//        val correctAnswer = questionBank[currentIndex].answer
+//        val messageResId = if (userAnswer == correctAnswer) {
+//            R.string.correct_toast
+//        } else {
+//            R.string.incorrect_toast
+//        }
+//        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+//            .show()
+//    }
+//
+        val correctAnswer=questionBank[currentIndex].answer
+        val messageAnswer=if (userAnswer == correctAnswer)R.string.correct_toast else R.string.incorrect_toast
+
+        val toast1=Toast.makeText(this,getString(messageAnswer),Toast.LENGTH_SHORT)
+        val _toast1:TextView = toast1.view!!.findViewById(android.R.id.message)
+        toast1.setGravity(Gravity.CENTER,5,5)
+        if (userAnswer==correctAnswer){
+            _toast1.setTextColor(Color.BLUE)
+            _toast1.setTextSize(24F)
+            toast1.show()
+        }else{
+            _toast1.setTextColor(Color.RED)
+            _toast1.setTextSize(24F)
+            toast1.show()
+        } }
+    fun showToase(message:Int){
     }
+
+
+
+
 }
